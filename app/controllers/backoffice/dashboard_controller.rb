9c788dadabdf8d971d7b05ab77dbdeda613db90a
ruan.nascimento <ruanmacambira@gmail.com>
2019-05-24 16:56:23 -0400

    Módulo 3, Aula 17
    - Adicionado barreira de segurança para acessar o backoffice
    - Opções de Login e Logout na dashboard do admin
    - Adicionado route 'backoffice', que tem o mesmo destino de 'backoffice/dashboard'

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Backoffice::DashboardController < ApplicationController
   layout "backoffice"
+  before_action :authenticate_admin!
   
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   namespace :backoffice do
     get 'dashboard', to: 'dashboard#index'
   end
+  
+  get 'backoffice', to: 'backoffice/dashboard#index'
   devise_for :admins
   devise_for :members
   get 'home/index'

c8bcd7ecc77fc6fe98e3e08a44fe0ea31ae6137f
ruan.nascimento <ruanmacambira@gmail.com>
2019-05-29 16:12:43 -0400

    Módulo 3, Aula 37
    - Concertando scope de admin (que não funcionaria para acesso restrito)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,13 @@
 class Admin < ApplicationRecord
+  #Enums
+  enum  role: { :full => 0, :restricted => 1}
+  
+  #Scopes
+  scope :with_full_access, -> { where(role: 0) }
+  scope :with_restricted_access, -> { where(role: 1) }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum  role: { :full => 0, :restricted => 1}
-  
-  scope :with_full_access, -> { where(role: 'full') }
-
 end
